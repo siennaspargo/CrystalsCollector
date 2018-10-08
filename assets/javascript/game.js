042789aebaa1c350fb0b6b5e4db8ac88d3518e22
SiennaS <ssienna48@gmail.com>
2018-10-08 00:16:57 -0400

    somme js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// TARGET NUMBER TO BE GUESSED
+//variable holding random number
+var targetNumber = 100;
+// jQuery selector statement calling target number
+$("#mystery-number").text(targetNumber);
+
+// USERS CURRENT SCORE
+var currentScore = 0;
+
+// RANDOM VALUES FOR CRYSTALS
+// array to hold crystal values
+var crystalValues = [5, 4, 8, 1];
+
